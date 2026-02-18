92459883a6f0c461596a2bd1cfeb582925483f54
Maksim Vabishchevich <oldvabik@gmail.com>
2026-02-18 16:03:25 +0300

    test commit

--- AuthService/src/main/java/org/oldvabik/authservice/exception/AlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package org.oldvabik.authservice.exception;
-
-public class AlreadyExistsException extends RuntimeException {
-    public AlreadyExistsException(String message) {
-        super(message);
-    }
-}
